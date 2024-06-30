ea657dbc06e22d2c45497c28f0ad2b33a3ab5d5c
taisan <131413638+taisan11@users.noreply.github.com>
2024-06-30 10:01:27 +0900

    add db_base

---
+++ src/schema.ts
@@ -0,0 +1,15 @@
+import { sql } from "drizzle-orm";
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
+
+export const todos = sqliteTable("todos", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  status: text("status", { enum: ["todo", "doing", "done"] }).default("todo"),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`
+  ),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`
+  ),
+});
+
