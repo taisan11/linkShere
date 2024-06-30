cf251033c04d8e518cd24bdc993f6942493c117d
taisan <131413638+taisan11@users.noreply.github.com>
2024-06-30 10:28:33 +0900

    1 hour

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,13 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('url', async (c) => {
+  //@ts-ignore
+  if (c.req.header("nb6rytc7tgn789") !== c.env.Token) {
+    return c.text('Hello Hono!')
+  }
+  const body = await c.req.json()
+  
+})
+
 export default app
